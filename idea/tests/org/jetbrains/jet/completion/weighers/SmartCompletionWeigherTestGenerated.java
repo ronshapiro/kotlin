7b7d4986d9755f6d9a1bdc3ab03ea04745424ec1
Valentin Kipyatkov <valentin@jetbrains.com>
2014-10-20 17:18:00 +0400

    KT-6028 Smart completion items priority
     #KT-6028 Fixed

--- idea/tests/org/jetbrains/jet/completion/weighers/SmartCompletionWeigherTestGenerated.java
+++ idea/tests/org/jetbrains/jet/completion/weighers/SmartCompletionWeigherTestGenerated.java
@@ -17,13 +17,9 @@
 package org.jetbrains.jet.completion.weighers;
 
 import com.intellij.testFramework.TestDataPath;
-import junit.framework.Test;
-import junit.framework.TestSuite;
-import org.junit.runner.RunWith;
 import org.jetbrains.jet.JetTestUtils;
-import org.jetbrains.jet.test.InnerTestClasses;
 import org.jetbrains.jet.test.TestMetadata;
-import org.jetbrains.jet.JUnit3RunnerWithInners;
+import org.junit.runner.RunWith;
 
 import java.io.File;
 import java.util.regex.Pattern;
@@ -161,6 +157,18 @@ public class SmartCompletionWeigherTestGenerated extends AbstractSmartCompletion
     @TestMetadata("NameSimilaritySorterPlacement.kt")
     public void testNameSimilaritySorterPlacement() throws Exception {
         String fileName = JetTestUtils.navigationMetadata("idea/testData/completion/weighers/smart/NameSimilaritySorterPlacement.kt");
+        doTest(fileName);
+    }
+    
+    @TestMetadata("SmartPriority.kt")
+    public void testSmartPriority() throws Exception {
+        String fileName = JetTestUtils.navigationMetadata("idea/testData/completion/weighers/smart/SmartPriority.kt");
+        doTest(fileName);
+    }
+    
+    @TestMetadata("SmartPriority2.kt")
+    public void testSmartPriority2() throws Exception {
+        String fileName = JetTestUtils.navigationMetadata("idea/testData/completion/weighers/smart/SmartPriority2.kt");
         doTest(fileName);
     }
     
